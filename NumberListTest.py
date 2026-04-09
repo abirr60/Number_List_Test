bf936af44ca1fd907b7eab2449c2465d1c25a8cd
abirr60 <abirmahamudmobin@gmail.com>
2026-04-10 00:28:25 +0600

    Implement random_list function to generate random numbers

---
+++ NumberListTest.py
@@ -0,0 +1,28 @@
+# CSP1150 Assignment - Student Number List Test
+# Author: Abur Mobin
+# Student Number: 10714721
+#
+# Description:
+# This program runs a number-list quiz using Easy, Medium, or Hard difficulty.
+# It includes the two required functions:
+#   1. random_list(quantity, minimum, maximum)
+#   2. ask_question(question, answer)
+#
+# References:
+# Python random module documentation:
+# https://docs.python.org/3/library/random.html
+# Python time module documentation:
+# https://docs.python.org/3/library/time.html
+
+import random
+import time
+
+
+def random_list(quantity, minimum, maximum):
+    numbers = []
+
+    for _ in range(quantity):
+        numbers.append(random.randint(minimum, maximum))
+
+    return numbers
+
